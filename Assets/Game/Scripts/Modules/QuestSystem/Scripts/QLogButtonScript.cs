5a5d2d193e7a5ee37f1b5f65125e2d290e14206f
F. Rick Reich <frickreich@gmail.com>
2020-01-04 17:24:39 +0100

    Add QuestSystem Module

---
+++ Assets/Game/Scripts/Modules/QuestSystem/Scripts/QLogButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Game.QuestSystem
+{
+	public class QLogButtonScript : MonoBehaviour
+	{
+
+		public int questID;
+		//public string questText;
+		public Text myText;
+
+		public void ShowAllInfos()// on click event for the left buttons
+		{
+			QuestManager.Instance.ShowQuestLog(questID);
+		}
+
+		public void ClosePanel()
+		{
+			QuestUIManager.Instance.HideQuestLogPanel();
+
+		}
+	}
+}
