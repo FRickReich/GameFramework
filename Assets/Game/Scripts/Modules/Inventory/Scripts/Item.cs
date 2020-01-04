fec6db1a734412eee7340aa7cb744b64394f1518
F. Rick Reich <frickreich@gmail.com>
2020-01-04 17:23:27 +0100

    Add Inventory Module

---
+++ Assets/Game/Scripts/Modules/Inventory/Scripts/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Inventory
+{
+	public class Item : ScriptableObject
+	{
+		public int id;
+		public string title;
+		public string identifier;
+		public string description;
+		public Sprite icon;
+
+		public Dictionary<string, int> stats = new Dictionary<string, int>();
+
+		public Item(int id, string title, string description, string identifier, Dictionary<string, int> stats)
+		{
+			this.id = id;
+			this.title = title;
+			this.description = description;
+			this.identifier = identifier;
+			this.icon = Resources.Load<Sprite>($"Sprites/Items/{ identifier }");
+		}
+
+		public Item(Item item)
+		{
+			this.id = item.id;
+			this.title = item.title;
+			this.description = item.description;
+			this.icon = Resources.Load<Sprite>($"Sprites/Items/{item.identifier}");
+		}
+    }
+}
---
+++ Assets/Game/Scripts/Modules/Inventory/Test/InventoryTester.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Game.Inventory;
+
+namespace Game.Test
+{
+	public class InventoryTester : MonoBehaviour
+	{
+		public List<Item> inventoryItems = new List<Item>();
+		public ItemDatabase itemDatabase;
+
+		public InventoryUI inventoryUI;
+
+		public void Start()
+		{
+			//AddItem("Diamond Sword");
+			AddItem(0);
+			AddItem(1);
+			AddItem(2);
+			//RemoveItem(1);
+		}
+
+		public void AddItem(int id)
+		{
+			Item itemToAdd = itemDatabase.GetItem(id);
+			inventoryItems.Add(itemToAdd);
+			inventoryUI.AddNewItem(itemToAdd);
+			Debug.Log($"Added item: {itemToAdd.title}");
+		}
+
+		public void AddItem(string itemName)
+		{
+			Item itemToAdd = itemDatabase.GetItem(itemName);
+			inventoryItems.Add(itemToAdd);
+
+			Debug.Log($"Added item: {itemToAdd.title}");
+		}
+
+		public Item FindItem(int id)
+		{
+			return inventoryItems.Find(item => item.id == id);
+		}
+
+		public void RemoveItem(int id)
+		{
+			Item itemToRemove = FindItem(id);
+
+			if(itemToRemove != null)
+			{
+				inventoryItems.Remove(itemToRemove);
+				inventoryUI.RemoveItem(itemToRemove);
+				Debug.Log($"Removed item: {itemToRemove.title}");
+			}
+		}
+	}
+}
