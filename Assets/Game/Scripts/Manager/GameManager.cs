63fefb7c8dc71988f34a81a645f3fef76384c8db
F. Rick Reich <frickreich@gmail.com>
2020-01-04 17:22:21 +0100

    Add managers

---
+++ Assets/Game/Scripts/Manager/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Game.Base;
+using Game.Helper;
+using Game.FSM;
+using Game.FSM.State;
+using Game.Internationalization;
+
+namespace Game
+{
+	public class GameManager : SingletonPersistent<GameManager>
+	{
+		private FiniteStateMachine stateManager = new FiniteStateMachine();
+
+		public bool debugMode;
+		public bool gamePauseMode;
+		public string gameMode;
+		public float gameTime;
+		public Timer gameTimer;
+		public Languages gameLanguage;
+
+		void Start()
+		{
+			this.stateManager.DebugMode(debugMode);
+
+			gameTimer = new Timer();
+
+			this.stateManager.ChangeState(new GamePrepareState(stateManager));
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			this.stateManager.ExecuteStateUpdate();
+
+			gameMode = stateManager.GetCurrentStateName();
+			gameTime = gameTimer.elapsed;
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Manager/SaveGameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Game.Base;
+using Game.SaveGame;
+using Game.FSM;
+using Game.FSM.State;
+
+namespace Game
+{
+	public class SaveGameManager : SingletonPersistent<SaveGameManager>
+	{
+		private FiniteStateMachine stateManager = new FiniteStateMachine();
+
+		public int currentBlock = 0;
+		public string currentLevel = "Game";
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			this.stateManager.ChangeState(new SaveLoadIdleState(stateManager));
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			this.stateManager.ExecuteStateUpdate();
+		}
+
+		public void LoadGame()
+		{
+			this.stateManager.ChangeState(new SaveLoadLoadingState(stateManager));
+		}
+
+		public void SaveGame()
+		{
+			this.stateManager.ChangeState(new SaveLoadSavingState(stateManager));
+		}
+	}
+}
